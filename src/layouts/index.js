77ba1a61d8049aa4e5ae022ce31650ad37e3aaaa
Ola Vea <ola@olavea.com>
2018-06-26 13:29:44 +0200

    Update index.js

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-export default ({ children }) => (
+export default ({ children }) =>
   <div style={{ margin: `0 auto`, maxWidth: 650, padding: `0 1rem` }}>
+    <h3>MySagaSite</h3>
     {children()}
   </div>
-);
